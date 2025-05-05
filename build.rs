4f3920ad64c470303b4ab2981ca646d2ecf182c4
KeiraDiaz <nkeiradiaz@gmail.com>
2025-05-05 23:17:51 +0700

    Payment Service Implementation

--- build.rs
+++ build.rs
@@ -1,6 +1,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_server(true)
+        .protoc_arg("--experimental_allow_proto3_optional")
         .compile(
             &["proto/services.proto"], // Path to your proto file
             &["proto"],                // Directory where the proto file is located
